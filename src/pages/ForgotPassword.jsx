7cc126855f46990b5878f9c4a94ac4430d9199af
Geen-19 <angelinedias53@gmail.com>
2023-01-10 21:31:52 +0530

    Changes in the Forgot Password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -10,7 +10,7 @@ export default function ForgotPassword() {
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
-      <div className='flex justify-center flex-wrap items-center px-6 py-12  mx-auto'>
+      <div className='flex justify-center flex-wrap items-center mx-auto px-6 py-12 '>
         <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
           <img src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80" alt="pics"
           className='w-full rounded-2xl'/>
