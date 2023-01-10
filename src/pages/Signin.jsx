03ba926b7527d8c3fdbf877429009be8e0a6e644
Geen-19 <angelinedias53@gmail.com>
2023-01-10 15:10:18 +0530

    signIn Page modified

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import { useState } from 'react'
 export default function SignIn() {
+  const[formData, setFormData] = useState({
+    email:"",
+    password: "",
+  });
+  const {email, password} = formData;
+  function onChange(e){
+    console.log(e.target.value);
+  }
   return (
     <section>
-      <h1 className='text'>Sign In</h1>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12  mx-auto'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80" alt="pics"
+          className='w-full rounded-2xl'/>
+        </div>
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form >
+            <input className='w-full' type="email" id = "email"           
+                 value = {email} onChange = {onChange} />
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
